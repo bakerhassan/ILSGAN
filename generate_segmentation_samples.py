44cf873005714b87e85e961668c3a44142a8cc0f
bakerhassan <61099003+bakerhassan@users.noreply.github.com>
2025-06-07 22:35:08 -0400

    Update generate_segmentation_samples.py

--- generate_segmentation_samples.py
+++ generate_segmentation_samples.py
@@ -34,8 +34,9 @@ def save_layer(tensor, outdir, layer_name, filename):
         tensor = 2 * torch.cat([tensor, tensor, tensor], dim=0) - 1
 
     # img = (tensor.permute(1, 2, 0) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-    img = (tensor.squeeze().cpu() * 255).clip(0, 255).to(torch.uint8)
+    img = (tensor.cpu() * 255).clip(0, 255).to(torch.uint8)
     img = img.permute(1, 2, 0).numpy()
+    print(img.shape)
     Image.fromarray(img, mode='L').save(outfile)
 
 
